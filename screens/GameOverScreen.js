9ba4b26a4ee6c413344bd0733adc5270df02146a
afiqaimanafr <aaafr98@gmail.com>
2022-04-01 05:44:59 +0800

    update: latest

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -12,7 +12,10 @@ function GameOverScreen() {
           source={require("../assets/images/success.png")}
         />
       </View>
-      <Text>Your phone needed X rounds to guess the number Y.</Text>
+      <Text style={styles.summaryText}>
+        Your phone needed <Text style={styles.highlight}>X</Text> rounds to
+        guess the number <Text style={styles.highlight}>Y</Text>.
+      </Text>
     </View>
   );
 }
@@ -38,5 +41,13 @@ const styles = StyleSheet.create({
   image: {
     width: "100%",
     height: "100%",
+  },
+  summaryText: {
+    fontFamily: "open-sans",
+    fontSize: 24,
+  },
+  highlight: {
+    fontFamily: "open-sans-bold",
+    color: Colors.primary500,
   },
 });
