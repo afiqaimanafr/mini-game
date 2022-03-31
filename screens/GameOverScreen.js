e12531dabc5dcc0432b19bc75b12eb22c82343c1
afiqaimanafr <aaafr98@gmail.com>
2022-03-31 22:32:10 +0800

    update: latest

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,7 +1,35 @@
-import { Text } from "react-native";
+import { View, Image, StyleSheet } from "react-native";
+import Colors from "../constants/colors";
+import Title from "../components/ui/Title";
 
 function GameOverScreen() {
-  return <Text>Game is over!</Text>;
+  return (
+    <View>
+      <Title>GAME OVER!</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/success.png")}
+        />
+      </View>
+    </View>
+  );
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    height: 300,
+    width: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: "hidden",
+    margin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
