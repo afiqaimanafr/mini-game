c7aec70291f6e34c21678bba6399496a94d568f4
afiqaimanafr <aaafr98@gmail.com>
2022-03-31 12:36:44 +0800

    update: latest

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, ImageBackground } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
     <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
-      <StartGameScreen />
+      <ImageBackground
+        source={require("./assets/images/background.png")}
+        resizeMode="cover"
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
