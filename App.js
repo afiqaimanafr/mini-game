be35b724cf102742a036b63f895765b59aac06b1
afiqaimanafr <aaafr98@gmail.com>
2022-03-31 15:52:05 +0800

    update: latest

--- App.js
+++ App.js
@@ -4,18 +4,34 @@ import { LinearGradient } from "expo-linear-gradient";
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
 import Colors from "./constants/colors";
+import GameOverScreen from "./screens/GameOverScreen";
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
+  const [gameIsOver, setGameIsOver] = useState(true);
 
   function pickedNumberHandler(pickedNumber) {
     setUserNumber(pickedNumber);
+    setGameIsOver(false);
   }
 
-  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
+  function gameOverHandler() {
+    setGameIsOver(true);
+  }
+
+  let screen = (
+    <StartGameScreen
+      onPickNumber={pickedNumberHandler}
+      onGameOver={gameOverHandler}
+    />
+  );
 
   if (userNumber) {
     screen = <GameScreen userNumber={userNumber} />;
+  }
+
+  if (gameIsOver && userNumber) {
+    screen = <GameOverScreen />;
   }
 
   return (
