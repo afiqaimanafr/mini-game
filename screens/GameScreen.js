6690ef3b94ec78cad0384a4e7228e9cb88eb0017
afiqaimanafr <aaafr98@gmail.com>
2022-03-31 15:08:27 +0800

    update: latest

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
-import Title from "../components/Title";
+import Title from "../components/ui/Title";
 
 function generateRandomBetween(min, max, exclude) {
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -12,13 +12,14 @@ function generateRandomBetween(min, max, exclude) {
   }
 }
 
-function GameScreen() {
-  const initialGuess = generateRandomBetween(1, 100);
-  const [currentGuess, setCurrentGuess] = useState();
+function GameScreen({ userNumber }) {
+  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
 
   return (
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
+      <Text></Text>
       <View>
         <Text>Higher or lower?</Text>
       </View>
