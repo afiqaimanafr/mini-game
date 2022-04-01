e36255040aa659c865a1844dc156556b5b3aa922
afiqaimanafr <aaafr98@gmail.com>
2022-04-01 17:58:02 +0800

    latest

--- components/ui/Title.ios.js
+++ components/ui/Title.ios.js
@@ -14,8 +14,6 @@ const styles = StyleSheet.create({
     textAlign: "center",
     // borderWidth: Platform.OS === "android" ? 2 : 0,
     // borderWidth: Platform.select({ ios: 0, android: 2 }),
-    borderWidth: 0,
-    borderColor: "white",
     padding: 12,
     maxWidth: "80%",
     width: 300,
